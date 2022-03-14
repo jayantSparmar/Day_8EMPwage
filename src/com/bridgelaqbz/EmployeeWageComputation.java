e9366ba1f631dd11a0671c520608829b64120983
jayant <jayantparmar420@gmail.com>
2022-03-14 10:13:51 +0530

    Added employee daily wage

--- src/com/bridgelaqbz/EmployeeWageComputation.java
+++ src/com/bridgelaqbz/EmployeeWageComputation.java
@@ -4,6 +4,7 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
     EmployeeWageComputation object = new EmployeeWageComputation();
     object.Uc1();
+    object.Uc2();
 }
 
     public void Uc1(){
@@ -15,7 +16,24 @@ public class EmployeeWageComputation {
         else
             System.out.println("Employee is Absent");
     }
+    public void Uc2(){
+        int isFullTime = 1;
+        int empRatePerHour = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == isFullTime)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * empRatePerHour;
+        System.out.println("Emp Wage: " + empWage);
+
+    }
 }
+
+
 
 
 
