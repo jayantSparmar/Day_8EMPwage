82a1d8dbebe08d3beae5eb7bd1504e473728cfbe
jayant <jayantparmar420@gmail.com>
2022-03-14 10:01:58 +0530

    Added employee wage program

---
+++ src/com/bridgelaqbz/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelaqbz;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to the employee wage program by using oops");
+        
+    }
+}
